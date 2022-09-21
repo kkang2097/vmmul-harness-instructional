98107a20d73681b3f406c2df4ac811a74ef77cd0
Elliot Kang <kkang2097@gmail.com>
2022-09-20 22:53:06 -0700

    Update dgemv-basic.cpp

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -10,8 +10,12 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of basic matrix multiply
 
    //Then add our triple for-loop
+
+   //Rows
    for (int i = 0; i < n; i++){
        y[i] = 0;
+
+      //Cols
      for(int j = 0; j < n; j++){
        //C[i*n + j] = 0;
         y[i] = y[i] + A[j*n + i] * x[j];
