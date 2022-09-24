f78d6012ef8e113e598dbcf7fb1e7c8881dee697
Elliot Kang <kkang2097@gmail.com>
2022-09-23 20:16:44 -0700

    comment out block

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -16,9 +16,10 @@ void my_dgemv(int n, double* A, double* x, double* y) {
 
    #pragma omp parallel
    {
-      int nthreads = omp_get_num_threads();
-      int thread_id = omp_get_thread_num();
-      printf("Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
+
+      // int nthreads = omp_get_num_threads();
+      // int thread_id = omp_get_thread_num();
+      // printf("Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
 
       //Then add our MVM
       for (int i = 0; i < n; i++){
